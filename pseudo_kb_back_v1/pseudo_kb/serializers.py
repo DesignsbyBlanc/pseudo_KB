c7c9e1be7ee95900b4e5a86317f2edacd2056221
Terry Blanc <teblanc2@gmail.com>
2024-06-14 21:09:49 -0400

    starting fresh on branch v2a

--- pseudo_kb_back_v1/pseudo_kb/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import BlockNote
-
-class BlockNoteSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BlockNote
-        fields = ["id", "title", "user_id", "content", "created"]
